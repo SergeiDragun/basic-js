472cde026847ea6ec38e128e52ec402f04169115
Siarhei <siarhei.drahun@gmail.com>
2022-10-14 15:41:39 +0300

    init: start what-seasons task

--- src/what-season.js
+++ src/what-season.js
@@ -11,10 +11,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function getSeason(springDate) {
+  if (typeof springDate == 'undefined') {
+    return 'Unable to determine the time of year!'
+  }
+
+  if (springDate.hasOwnProperty("getMonth")) {
+      throw new Error("Invalid date!")
+  }
+  
+  try  {
+    (springDate.getMonth())
+  } catch {
+    throw new Error("Invalid date!")
+  }
+
+  if(springDate.getMonth() == 0 || springDate.getMonth() == 1 || springDate.getMonth() == 11) {
+      return "winter"
+  }
+
+  else if(springDate.getMonth() == 2 || springDate.getMonth() == 3 || springDate.getMonth() == 4) {
+      return "spring"
+  }
+
+  else if(springDate.getMonth() == 5 || springDate.getMonth() == 6 || springDate.getMonth() == 7) {
+      return "summer"
+  }
+
+  else if(springDate.getMonth() == 8 || springDate.getMonth() == 9 || springDate.getMonth() == 10) {
+      return "fall"
+  } 
+  }
 
 module.exports = {
   getSeason
